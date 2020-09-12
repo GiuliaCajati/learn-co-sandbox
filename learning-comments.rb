315646fd1c8d56d105804d6589b713448d8c6d3e
Giulia Cajati <giuliacajati@gmail.com>
2020-09-12 18:51:21 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
